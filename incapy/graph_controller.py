73470c46ec1d8d8defb4b3f5c924d527f972b0a8
Björn Müller <bj.mueller@fz-juelich.de>
2019-01-08 15:00:25 +0100

    Added generation of random data for graph
    First implementation does not load data, only generates some random
    values

--- incapy/graph_controller.py
+++ incapy/graph_controller.py
@@ -1,8 +1,11 @@
 
-
+from incapy.load_data import load_data_random
 from incapy.icontroller import IController
 
 
 class GraphAlgorithm(IController):
-    raise NotImplementedError()
+    def __init__(self, model):
+        super().__init__(model)
+        load_data_random(model, 10)
+
 
--- incapy/load_data.py
+++ incapy/load_data.py
@@ -1,6 +1,14 @@
+import numpy as np
 
 
 def load_data(graph):
     # load the data from the hdf5 files (2-dimensional weighted matrix)
     # and populate the empty graph
     raise NotImplementedError()
+
+
+def load_data_random(model, num_nodes):
+    node_indices = np.arange(num_nodes)
+    model.set_ids(node_indices)
+    positions = np.random.rand(2, num_nodes)
+    model.set_positions(positions)
